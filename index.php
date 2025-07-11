3e56d68e2be3f636e8ca05eb17146873dcfe0aab
Chathura Kulathunga <chathurakulathunga.foxc@gmail.com>
2025-07-11 10:39:10 +0530

    dev-6-footer added

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 
 <body>
@@ -91,6 +92,7 @@
 
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <script src="assets/js/script.js"></script>
+  <?php include 'includes/footer.php'; ?>
 </body>
 
 </html>
